e811b14d6a4b04e59d09aa9465c3e8a275b5bd8c
dangdle <dangdle@hotmail.co.nz>
2014-05-07 09:26:46 +1200

    Update MainMenuActivity.java

--- src/com/example/musicbuttons/MainMenuActivity.java
+++ src/com/example/musicbuttons/MainMenuActivity.java
@@ -22,7 +22,6 @@ public class MainMenuActivity extends Activity {
 
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
-		System.out.println();
 		return true;
 	}
 	
